0b05773f3880726fb1cfd3a47657cdc623f14194
Duck2504 <giaduc25042006@gmail.com>
2025-09-08 13:58:17 +0700

    Lam bai 1 va 2

---
+++ Thực hành 1/Bai1/Bai1/Bai1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while (T--) {
+		string s;
+		cin >> s;
+		map<string, int> count;
+		int n = s.size();
+		for (int i = 0; i + 2 < n; i++) {
+			string boBa = s.substr(i, 3);
+			count[boBa]++;
+		}
+		int kq = 0;
+		for (auto& p : count) {
+			kq = max(kq, p.second);
+		}
+		cout << kq << endl;
+	}
+	return 0;
+}
---
+++ Thực hành 1/Bai2/Bai2/Bai2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int>a(n);
+	vector<bool>check(n+1, false);
+	for (int i=0; i < n; i++) {
+		cin >> a[i];
+	}
+	bool yes = true;
+	for (int i = 0; i < n; i++) {
+		int temp = a[i];
+		if (temp < 1 || temp >n) {
+			yes = false;
+			break;
+		}
+		if (check[temp]) {
+			yes = false;
+			break;
+		}
+		check[temp] = true;
+	}
+	if (yes) cout << "YES";
+	else cout << "NO";
+	return 0;
+}
